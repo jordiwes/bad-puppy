d49cdea5acabbfb25c86ba449a84489ae80b8bf7
jordiwes <jordiwes@gmail.com>
2016-03-17 10:10:22 -0700

    this is going to the branch

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -3,6 +3,7 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * stepj
+ * branch comment
  * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
  * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
